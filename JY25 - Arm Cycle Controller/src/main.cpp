284643dcaa0a726d6b87d830d14cf968d0079e7d
FabianLCH <fabian.lozanoc23@gmail.com>
2021-03-05 21:21:24 -0500

    Add MPU code and library dependencies

--- JY25 - Arm Cycle Controller/src/main.cpp
+++ JY25 - Arm Cycle Controller/src/main.cpp
@@ -1,6 +1,9 @@
 // INCLUDE LIBRARIES
 #include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 #include <Encoder.h>
+#include <Wire.h>
 
 // DEFINITIIONS ---------------------------------------------------------
 #define INDEX_PULSE 21
@@ -35,6 +38,8 @@ IntervalTimer speedTimer;
 // ENCODER OBJECTS ------------------------------------------------------
 Encoder ArmCycle(ENCODER_PIN_A, ENCODER_PIN_B);
 elapsedMicros sinceEncoderUpdate;
+
+Adafruit_MPU6050 mpu;
 
 // GLOBAL VARIABLES -----------------------------------------------------
 volatile long encoderPosition  = -999;
@@ -131,6 +136,86 @@ void setup() {
 
     // initialize serial communication at 115200 bits per second
     Serial.begin(115200);
+
+    while (!Serial)
+        delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+    // attempt to initialize the MPU
+    if (!mpu.begin()) {
+        Serial.println("Failed to find MPU6050 chip");
+
+        // while (1) {
+        //     delay(10);
+        // }
+    }
+    else {
+        Serial.println("MPU6050 Found!");
+
+        mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+        Serial.print("Accelerometer range set to: ");
+        switch (mpu.getAccelerometerRange()) {
+        case MPU6050_RANGE_2_G:
+            Serial.println("+-2G");
+            break;
+        case MPU6050_RANGE_4_G:
+            Serial.println("+-4G");
+            break;
+        case MPU6050_RANGE_8_G:
+            Serial.println("+-8G");
+            break;
+        case MPU6050_RANGE_16_G:
+            Serial.println("+-16G");
+            break;
+        }
+
+        mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+        Serial.print("Gyro range set to: ");
+        switch (mpu.getGyroRange()) {
+        case MPU6050_RANGE_250_DEG:
+            Serial.println("+- 250 deg/s");
+            break;
+        case MPU6050_RANGE_500_DEG:
+            Serial.println("+- 500 deg/s");
+            break;
+        case MPU6050_RANGE_1000_DEG:
+            Serial.println("+- 1000 deg/s");
+            break;
+        case MPU6050_RANGE_2000_DEG:
+            Serial.println("+- 2000 deg/s");
+            break;
+        }
+
+        mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+        Serial.print("Filter bandwidth set to: ");
+        switch (mpu.getFilterBandwidth()) {
+        case MPU6050_BAND_260_HZ:
+            Serial.println("260 Hz");
+            break;
+        case MPU6050_BAND_184_HZ:
+            Serial.println("184 Hz");
+            break;
+        case MPU6050_BAND_94_HZ:
+            Serial.println("94 Hz");
+            break;
+        case MPU6050_BAND_44_HZ:
+            Serial.println("44 Hz");
+            break;
+        case MPU6050_BAND_21_HZ:
+            Serial.println("21 Hz");
+            break;
+        case MPU6050_BAND_10_HZ:
+            Serial.println("10 Hz");
+            break;
+        case MPU6050_BAND_5_HZ:
+            Serial.println("5 Hz");
+            break;
+        }
+
+        Serial.println("");
+        delay(100);
+    }
+
+    
 }
 
 /**
@@ -162,6 +247,33 @@ void loop() {
     long newEncoderPosition;
     double PPS, CPS, armCycleCPS;
 
+    sensors_event_t a, g, temp;
+
+    mpu.getEvent(&a, &g, &temp);
+
+    // print out MPU readings
+    Serial.print("Acceleration X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.println(" m/s^2");
+
+    Serial.print("Rotation X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad/s");
+
+    Serial.print("Temperature: ");
+    Serial.print(temp.temperature);
+    Serial.println(" degC");
+
+    Serial.println("");
+
     newEncoderPosition = ArmCycle.read();
 
     if (newEncoderPosition != encoderPosition) {
@@ -192,4 +304,6 @@ void loop() {
         // Reset time between encoder increments
         sinceEncoderUpdate = 0;
     }
+
+    delay(500);
 }
